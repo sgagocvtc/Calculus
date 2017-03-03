451f462be1d6fe7b737830efc99143dc99b686bc
Unknown <sgago@cvtc.edu>
2017-03-03 11:52:31 -0600

    Added initial files
    Added Calculus.js and index.html files

---
+++ Calculus.js
@@ -0,0 +1,70 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2017 Steven Gago
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ * TODO: We need
+ * 1. Lots of comments
+ * 2. Arrays
+ * 3. If statements
+ * 4. Constants
+ * 5. Validate user input
+ */
+var myFunction = new Function("x", "return x * x");
+
+function isValidEquation(equation) {
+    'use strict';
+    
+    var isValid = false;
+    var func = new Function("x", equation);
+    
+    isValid = typeof(func(2)) === "integer";
+    
+    return isValid;
+}
+
+function integrate(from, to, step) {
+    'use strict';
+    
+    var x = 0;
+    var rectangleArea = 0;
+    var totalArea = 0;
+    var rectangles = Math.abs(to - from) / step;
+
+    for (var i = 0; i < rectangles; i++) {
+        // Calculate the new X value
+        x = i * step + from;
+        
+        // Calculate the rectangle's area (x * y)
+        rectangleArea = Math.abs(step * myFunction(x));
+        
+        // Add the rectangles area to the total area under the curve
+        totalArea += rectangleArea;
+    }
+  
+    return totalArea;
+}
+
+function derive(at, step) {
+    'use strict';
+    
+    return (myFunction(at + step) - myFunction(at - step)) / (2 * step);
+}
